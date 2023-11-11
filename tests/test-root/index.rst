86fa5500996d8e0194d1473a2b8a92b113a46cb3
Kazuya Takei <myself@attakei.net>
2023-11-11 20:20:37 +0900

    feat: Generate roles to render color-text with style attribute

--- tests/test-root/index.rst
+++ tests/test-root/index.rst
@@ -1,2 +1,4 @@
 Test doc for atsphinx-color-text
 ================================
+
+sphinx-revealjs is presentation library for Pythonista using reStructuredText and :color:red:`Sphinx`.
--- tests/test_it.py
+++ tests/test_it.py
@@ -2,9 +2,15 @@
 from io import StringIO
 
 import pytest
+from bs4 import BeautifulSoup
 from sphinx.testing.util import SphinxTestApp
 
 
 @pytest.mark.sphinx("html")
 def test__it(app: SphinxTestApp, status: StringIO, warning: StringIO):
     """Test to pass."""
+    app.build()
+    html = app.outdir / "index.html"
+    soup = BeautifulSoup(html.read_text(), "html.parser")
+    span = soup.span
+    assert "style" in span.attrs
